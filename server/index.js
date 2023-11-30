aac25132944af971a5a415bf86cab90054eae95f
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2023-11-30 12:57:47 +0530

    learning graphql

---
+++ server/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const colors = require("colors");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// connect to database
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(PORT, console.log("Server running on :", PORT));
